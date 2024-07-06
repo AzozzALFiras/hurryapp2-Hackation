2f887873dec21e37443735fa68294d75769b4730
Ahmed K Sameen <ahmedsameen.sec@gmail.com>
2024-07-06 14:41:11 +0300

    Update db.js

--- nodejs/db.js
+++ nodejs/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'hurryapp2',
-    password: 'x2sBYyLeSJ4rAneD',
+    password: '',
     database: 'hurryapp2'
 });
 
